096243cf06bba54a40b6cfc53ae4e5aa8ed1da91
manuelp1345 <manuelp1345@gmail.com>
2021-01-19 18:38:54 -0400

    Validation input Js

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,37 @@
+form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    search()
+})
+
 function search() {
     let searchBar = document.getElementById("search").value,
         contenedor = document.querySelector(".contenido")
 
-    contenedor.innerHTML = `<div class="align-items-center mt-5 spinner-border" role="status"></div>`
+    if (searchBar == "") {
+        contenedor.innerHTML = `<div class="col-12 alert alert-danger" role="alert">
+        Por favor rellene el campo 
+      </div>`
+
+    } else {
+        contenedor.innerHTML = `<div class="align-items-center mt-5 spinner-border" role="status"></div>`
 
 
-    fetch(`api/${searchBar}`).then(e => e.json())
-        .then(e => {
-            let enlaces = e.enlaces,
-                titulo = e.titulo
-            precio = e.precio,
-                img = e.img
 
-            enlaces.length = (enlaces.length / 2) + 3
-            console.log(enlaces.length);
-            contenedor.innerHTML = ""
-            let i = 0
-            for (let element of enlaces) {
-                contenedor.innerHTML += `
+        fetch(`api/${searchBar}`).then(e => e.json())
+            .then(e => {
+                let enlaces = e.enlaces,
+                    titulo = e.titulo
+                precio = e.precio,
+                    img = e.img
+
+                enlaces.length = (enlaces.length / 2) + 3
+                console.log(enlaces.length);
+                contenedor.innerHTML = ""
+                let i = 0
+                for (let element of enlaces) {
+                    contenedor.innerHTML += `
                 <div class="col-3 card text-center  m-3">
                 <a class="text-decoration-none " href="${element}">
                     <img class="img-fluid pt-3" width="300" src="${img[i]}" alt="">
@@ -25,7 +39,8 @@ function search() {
                     <p class="h5 precio font-weight-bold">${precio[i]} Bs</p>
                 </a>
             </div>`
-                i++
-            }
-        })
+                    i++
+                }
+            })
+    }
 }
